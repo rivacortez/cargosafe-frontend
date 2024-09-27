c8c72c8c84274e124c4ac4c3a7f97ee5c4698d1f
rivacortezf <u202215313@upc.edu.pe>
2024-09-27 01:23:24 -0500

    feat: add vehicle entity

--- src/app/vehicles/model/vehicles.entity.ts
+++ src/app/vehicles/model/vehicles.entity.ts
@@ -1,2 +1,20 @@
+import {model} from "@angular/core";
+
 export class VehiclesEntity {
+
+  id: number;
+  model: string;
+  plate: string;
+  max_load: string;
+  volume: string;
+  url_image: string;
+  constructor(vehicles:{id?: number, model?: string, plate?: string, max_load?: string, volume?: string, url_image?: string} ) {
+    this.id = vehicles.id || 0;
+    this.model = vehicles.model || '';
+    this.plate = vehicles.plate || '';
+    this.max_load = vehicles.max_load || '';
+    this.volume = vehicles.volume || '';
+    this.url_image = vehicles.url_image || '';
+  }
+
 }
